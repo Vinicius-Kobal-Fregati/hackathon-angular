f70600f1986f9002b520763862c5bbd894f1112a
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-02-23 11:37:11 -0300

    Elaboracao da pagina e funcionalidade de exibir aniversariantes

--- src/app/aniversariantes/aniversariantes.component.ts
+++ src/app/aniversariantes/aniversariantes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioSemSenhaDTO } from '../DTO/UsuarioSemSenhaDTO';
+import { ActivatedRoute } from '@angular/router';
+import { UsuarioService } from '../service/usuario.service';
 
 @Component({
   selector: 'app-aniversariantes',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AniversariantesComponent implements OnInit {
 
-  constructor() { }
+  id: any
+  usuarios: Array<UsuarioSemSenhaDTO> = []
+
+  constructor(private usuarioService: UsuarioService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(parametros => {
+      if (parametros['id']) {
+        this.id = parametros['id']
+        this.buscarAniversariantes()
+      }
+    })
   }
 
+  buscarAniversariantes = () => {
+    this.usuarioService.buscarAniversariantes(this.id).subscribe((usuario: Array<UsuarioSemSenhaDTO>) => {
+      if (usuario.length > 0) {
+        this.usuarios = usuario
+      } else {
+        alert("Nenhum aniversariante encontrado")
+      }
+    })
+  }
 }
